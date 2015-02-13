928d26861953f901d58e6e015727bb9bbe6e6e44
John Howard (VM) <John.Howard@microsoft.com>
2015-02-13 11:46:14 -0800

    api\server: Factored out UnixHttp on Windows, supported on Linux only.

---
+++ api/server/server_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package server
+
+import (
+	"fmt"
+	"github.com/docker/docker/engine"
+)
+
+// NewServer sets up the required Server and does protocol specific checking.
+func NewServer(proto, addr string, job *engine.Job) (Server, error) {
+	// Basic error and sanity checking
+	switch proto {
+	case "tcp":
+		return setupTcpHttp(addr, job)
+	default:
+		return nil, fmt.Errorf("Invalid protocol format. Windows only supports tcp.")
+	}
+}
